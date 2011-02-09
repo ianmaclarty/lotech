073f3b5929613f10a2538e6a208a2e36c655e961
mu@muandheyo.com <mu@muandheyo.com@cc13c597-6222-e281-3f33-90c271915fff>
2011-02-09 23:59:59 +0000

    Support different text alignments.
    git-svn-id: https://lotech.googlecode.com/svn/trunk@103 cc13c597-6222-e281-3f33-90c271915fff

--- src/ltlua/lttext.lua
+++ src/ltlua/lttext.lua
@@ -1,10 +1,12 @@
-function lt.Text(str, font)
+function lt.Text(str, font, halign, valign)
+    halign = halign or "left"
+    valign = valign or "center"
     local em = font.m or font.M or {w = 0.1, h = 0.1}
     local space = em.w * (font.space or 0.3)
     local hmove = em.w * (font.hmove or 0.05)
     local vmove = em.h * (font.vmove or 1.2)
     local kerntable = font.kern
-    local x, y, dx, k, gap = 0, 0, 0, 0, 0
+    local x, y, dx, k, gap = 0, -em.h / 2, 0, 0, 0
     local line = lt.Layer()
     local lines = {line}
     for i = 1, str:len() do
@@ -31,14 +33,55 @@ function lt.Text(str, font)
         end
     end
     line.w = x - gap
-    local layer = lt.Layer()
-    local boxwidth = 0
+    local haligned = lt.Layer()
+    -- Compute text bounding box.
+    local bb_w = 0
+    local bb_h = #lines * vmove - (vmove - em.h)
     for _, line in ipairs(lines) do
-        layer:Insert(line)
-        if line.w > boxwidth then
-            boxwidth = line.w
+        if line.w > bb_w then
+            bb_w = line.w
         end
     end
-    layer.w = boxwidth
-    return layer
+    local bb_l, bb_r
+    if halign == "left" then
+        bb_l = 0
+        bb_r = bb_w
+    elseif halign == "right" then
+        bb_l = -bb_w
+        bb_r = 0
+    else -- centered
+        bb_l = -bb_w / 2
+        bb_r = bb_w / 2
+    end
+    -- Insert lines into haligned layer.
+    for _, line in ipairs(lines) do
+        if halign == "left" then
+            haligned:Insert(line)
+        elseif halign == "right" then
+            haligned:Insert(lt.Translate(line, -line.w, 0))
+        elseif halign == "center" then
+            haligned:Insert(lt.Translate(line, -line.w / 2, 0))
+        end
+    end
+    local valigned
+    if valign == "top" then
+        valigned = haligned
+        bb_t = 0
+        bb_b = -bb_h
+    elseif valign == "bottom" then
+        valigned = lt.Translate(haligned, 0, bb_h)
+        bb_t = bb_h
+        bb_b = 0
+    else -- centered
+        valigned = lt.Translate(haligned, 0, bb_h / 2)
+        bb_t = bb_h / 2
+        bb_b = -bb_h / 2
+    end
+    valigned.w = bb_w
+    valigned.h = bb_h
+    valigned.l = bb_l
+    valigned.r = bb_r
+    valigned.t = bb_t
+    valigned.b = bb_b
+    return valigned
 end
--- tests/text.lua
+++ tests/text.lua
@@ -8,15 +8,18 @@ images.font.kern = {
     Wo = 0.01,
     Vo = 0.005,
     oV = 0.005,
+    Vj = -0.04,
     ["V."] = -0.15,
 }
 local main = lt.Layer()
 
-local text = lt.Text("Hello World!\nVooV.", images.font)
+local text = lt.Text("Hello World!\nVjoV.", images.font, "center", "center")
 local text_and_rect = lt.Layer()
 text_and_rect:Insert(text, 2)
-text_and_rect:Insert(lt.Tint(lt.Rect(0, 0.1, text.w, -0.1), 1, 0, 0, 0.5), 1)
-main:Insert(lt.Translate(lt.Scale(text_and_rect, 3), -0.5, 0))
+text_and_rect:Insert(lt.Tint(lt.Rect(text.l, text.b, text.r, text.t), 1, 0, 0, 0.5), 1)
+main:Insert(lt.Scale(text_and_rect, 3))
+main:Insert(lt.Line(0, -1, 0, 1), -1)
+main:Insert(lt.Line(-1, 0, 1, 0), -1)
 
 function lt.Render()
     main:Draw()
