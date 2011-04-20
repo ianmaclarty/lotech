9c50624b5792a116f733f98fdab8485c2a3d1960
Ian MacLarty <ian.maclarty@gmail.com>
2011-04-20 20:57:25 +1000

    fix wrong arg

--- src/lt/ltnet.cpp
+++ src/lt/ltnet.cpp
@@ -218,7 +218,7 @@ static int receive_msg(int sock, char **msg, int *n, char **errmsg) {
             }
         }
     }
-    checksum("received", *msg, len);
+    checksum("received", *msg, n);
     return 1;
 }
 
