3ecd51754c48407bac300355db10e35a1c19468e
Ian MacLarty <ian.maclarty@gmail.com>
2011-11-02 20:37:04 +1100

    use nearest texture filtering to avoid cracks between tiles

--- src/lt/ltimage.cpp
+++ src/lt/ltimage.cpp
@@ -61,8 +61,9 @@ LTAtlas::LTAtlas(LTImagePacker *packer, const char *dump_file) {
     }
     glGenTextures(1, &texture_id);
     glBindTexture(GL_TEXTURE_2D, texture_id);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // XXX Should make texture filtering configurable.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
     #ifdef LTIOS
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, buf->width, buf->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buf->bb_pixels);
