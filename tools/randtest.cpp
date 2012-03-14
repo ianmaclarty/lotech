a612b53e102213e4c663a2123a3e632f9f703bf7
Ian MacLarty <ian.maclarty@gmail.com>
2012-03-14 17:38:00 +1100

    test rand and random

--- tools/randtest.cpp
+++ tools/randtest.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include "ltrandom.h"
 #include "ltimage.h"
 
-#define SAMPLE_SIZE 1000000
+#define SAMPLE_SIZE 10000000
+//#define USERANDOM 1
 
 static void int_test(LTRandomGenerator *r, int n);
 static void bool_test(LTRandomGenerator *r);
@@ -11,8 +13,33 @@ static void rand_bw_image(LTRandomGenerator *r, int w, int h, const char *name);
 static void rand_color_image(LTRandomGenerator *r, int w, int h, const char *name);
 static void rand_scatter_image(LTRandomGenerator *r, int w, int h, const char *name);
 
+static int rand_next_int(int n);
+static bool rand_next_bool();
+static int random_next_int(int n);
+static bool random_next_bool();
+
+#if defined(USERAND)
+
+#define RINT(r, n) rand_next_int(n)
+#define RBOOL(r) rand_next_bool()
+
+#elif defined(USERANDOM)
+
+#define RINT(r, n) random_next_int(n)
+#define RBOOL(r) random_next_bool()
+
+#else
+
+#define RINT(r, n) r->nextInt(n)
+#define RBOOL(r) r->nextBool()
+
+#endif
+
 int main() {
-    LTRandomGenerator r(time(NULL));
+    unsigned int seed = time(NULL);
+    srand(seed);
+    srandom(seed);
+    LTRandomGenerator r((int)seed);
     int_test(&r, 1);
     int_test(&r, 2);
     int_test(&r, 3);
@@ -32,12 +59,14 @@ int main() {
         printf("Quick check FAILED\n");
     }
     rand_color_image(&r, 281, 281, "randimage1.png");
-    rand_bw_image(&r, 300, 451, "randimage2.png");
-    rand_color_image(&r, 1000, 500, "randimage3.png");
-    rand_bw_image(&r, 1000, 500, "randimage4.png");
-    rand_scatter_image(&r, 500, 500, "scatterimage5.png");
-    rand_scatter_image(&r, 281, 281, "scatterimage6.png");
-    rand_scatter_image(&r, 444, 431, "scatterimage7.png");
+    rand_bw_image(&r, 256, 256, "randimage2.png");
+    rand_color_image(&r, 512, 512, "randimage3.png");
+    rand_bw_image(&r, 64, 64, "randimage4.png");
+    rand_scatter_image(&r, 500, 500, "scatterimage1.png");
+    rand_scatter_image(&r, 281, 281, "scatterimage2.png");
+    rand_scatter_image(&r, 256, 256, "scatterimage3.png");
+    rand_scatter_image(&r, 128, 128, "scatterimage4.png");
+    rand_scatter_image(&r, 64, 64, "scatterimage5.png");
     return 0;
 }
 
@@ -47,7 +76,7 @@ static void int_test(LTRandomGenerator *r, int n) {
         icounts[i] = 0;
     }
     for (int i = 0; i < SAMPLE_SIZE; i++) {
-        icounts[r->nextInt(n)]++;
+        icounts[RINT(r, n)]++;
     }
     printf("N = %d:\n", n);
     for (int i = 0; i < n; i++) {
@@ -61,7 +90,7 @@ static void bool_test(LTRandomGenerator *r) {
     bcounts[0] = 0;
     bcounts[1] = 0;
     for (int i = 0; i < SAMPLE_SIZE; i++) {
-        bcounts[r->nextBool() ? 1 : 0]++;
+        bcounts[RBOOL(r) ? 1 : 0]++;
     }
     printf("Bool:\n");
     printf("  false: %d\n", bcounts[0]);
@@ -79,7 +108,7 @@ static void rand_bw_image(LTRandomGenerator *r, int w, int h, const char *name) 
     buf.bb_pixels = new LTpixel[w * h];
     for (int i = 0; i < w; i++) {
         for (int j = 0; j < h; j++) {
-            if (r->nextBool()) {
+            if (RBOOL(r)) {
                 buf.bb_pixels[j * w + i] = 0xFFFFFFFF;
             } else {
                 buf.bb_pixels[j * w + i] = 0x000000FF;
@@ -112,7 +141,7 @@ static void rand_color_image(LTRandomGenerator *r, int w, int h, const char *nam
     buf.bb_pixels = new LTpixel[w * h];
     for (int i = 0; i < w; i++) {
         for (int j = 0; j < h; j++) {
-            buf.bb_pixels[j * w + i] = colors[r->nextInt(ncols)];
+            buf.bb_pixels[j * w + i] = colors[RINT(r, ncols)];
         }
     }
     ltWriteImage(name, &buf);
@@ -131,8 +160,8 @@ static void rand_scatter_image(LTRandomGenerator *r, int w, int h, const char *n
     buf.bb_pixels = new LTpixel[w * h];
     memset(buf.bb_pixels, 0xFF, w * h * 4);
     for (int i = 0; i < n; i++) {
-        int x = r->nextInt(w);
-        int y = r->nextInt(h);
+        int x = RINT(r, w);
+        int y = RINT(r, h);
         int val = (buf.bb_pixels[y * w + x] & 0xFF000000) >> 24;
         if (val > 0) {
             val--;
@@ -141,4 +170,42 @@ static void rand_scatter_image(LTRandomGenerator *r, int w, int h, const char *n
     }
     ltWriteImage(name, &buf);
     printf("Generated %s\n", name);
+}
+
+static int rand_next_int(int n) {
+    int m = n - 1;
+    m |= m >> 1; 
+    m |= m >> 2; 
+    m |= m >> 4; 
+    m |= m >> 8; 
+    m |= m >> 16;
+    int r;
+    do {
+        r = rand() & m;
+    } while (r >= n);
+    return r;
+}
+
+static int random_next_int(int n) {
+    int m = n - 1;
+    m |= m >> 1; 
+    m |= m >> 2; 
+    m |= m >> 4; 
+    m |= m >> 8; 
+    m |= m >> 16;
+    int r;
+    do {
+        r = random() & m;
+    } while (r >= n);
+    return r;
+}
+
+bool rand_next_bool() {
+    static const int b = 1 << 27; // Pick a bit to use.
+    return rand() & b;
+}
+
+bool random_next_bool() {
+    static const int b = 1 << 27; // Pick a bit to use.
+    return random() & b;
 }
